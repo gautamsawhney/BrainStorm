78923c3c4e7dd81199b910c49a405e2552dcec50
gautam <gsawhney21@gmail.com>
2014-01-21 23:48:31 +0530

    added devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BrainStorm::Application.config.secret_token = 'f95a9f4c54054b228d1e4c514d4d1841c0f1ba8d3fa33bdf74b9f0ed058fa2da693d6c98cb8affefd17e54f38cdebf9474d22eb54d081a850663bcf543930d6a'
