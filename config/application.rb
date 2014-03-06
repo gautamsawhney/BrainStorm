b5fb97c073319ebc98ad2a2a04bf650cbbc9f8fc
gautam <gsawhney21@gmail.com>
2014-03-07 01:49:11 +0530

    Add precompiled assets for Heroku

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module BrainStorm
     config.assets.precompile = %w( *.js *.css )
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
-config.autoload_paths += %W(#{config.root}/lib #{config.root}/app/uploaders)
+    config.autoload_paths += %W(#{Rails.root}/app/uploaders)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
